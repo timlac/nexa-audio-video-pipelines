76371ce6c8edcbfb37ab4218ed0bbc2e2560568f
timlac <tim@lachmann.nu>
2024-02-19 13:49:59 +0100

    set up single file openpose

--- openpose/process_file.sh
+++ openpose/process_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define paths
-HOST_VIDEO_PATH="/home/tim/Work/nexa/nexa-audio-video-pipelines/data/test/openpose_example/video.avi"  # Full path to the input video on your host
+HOST_VIDEO_PATH="/home/tim/Work/nexa/nexa-audio-video-pipelines/data/test/sentimotion/A67_pea_v_3.mov"  # Full path to the input video on your host
 HOST_OUTPUT_DIR="/home/tim/Work/nexa/nexa-audio-video-pipelines/data/out/openpose/single"  # Directory where you want to save output on your host
 HOST_MODELS_DIR="/home/tim/Work/nexa/nexa-audio-video-pipelines/data/models/openpose/models/"  # Directory where your OpenPose models are located on your host
 CONTAINER_NAME="d0ckaaa/openpose"  # Name of the OpenPose Docker image
@@ -13,7 +13,9 @@ RENDER_POSE="0"
 WRITE_JSON="output/"  # Directory inside the container where JSON output will be saved
 
 # Start the container in detached mode and keep it running
-CONTAINER_ID=$(docker run -d --gpus all -v $(dirname "$HOST_VIDEO_PATH"):/input -v $HOST_OUTPUT_DIR:/output $CONTAINER_NAME /bin/bash -c "sleep infinity")
+CONTAINER_ID=$(docker run -d --gpus all --user $(id -u):$(id -g) -v $(dirname "$HOST_VIDEO_PATH"):/input \
+-v $HOST_OUTPUT_DIR:/output $CONTAINER_NAME /bin/bash -c "sleep infinity")
+
 
 echo $CONTAINER_ID
 
@@ -21,11 +23,10 @@ echo $CONTAINER_ID
 docker cp $HOST_MODELS_DIR $CONTAINER_ID:/opt/openpose/
 
 # Execute OpenPose within the container
-docker exec $CONTAINER_ID /bin/bash -c "cd /opt/openpose && ./build/examples/openpose/openpose.bin --video /input/'$(basename "$HOST_VIDEO_PATH")' --display $DISPLAY --render_pose $RENDER_POSE --net_resolution $NET_RESOLUTION --write_json /output"
+docker exec $CONTAINER_ID /bin/bash -c "cd /opt/openpose && ./build/examples/openpose/openpose.bin \
+--video /input/'$(basename "$HOST_VIDEO_PATH")' \
+--display $DISPLAY --render_pose $RENDER_POSE --net_resolution $NET_RESOLUTION --write_json /output"
 
-
-# Try changing ./build/examples/openpose/openpose.bin to ./build/src/openpose/libopenpose.so.1.7.0
-# How can I run it not in demo mode?
 
 # Stop and remove the container
 docker stop $CONTAINER_ID

d0e519c77044c48f23e5c1a31d239846a2cd463e
timlac <tim@lachmann.nu>
2024-02-22 16:20:23 +0100

    added prints

--- opensmile_pipes/process_file.py
+++ opensmile_pipes/process_file.py
@@ -13,8 +13,11 @@ smile = opensmile.Smile(
     feature_level=opensmile.FeatureLevel.LowLevelDescriptors,
 )
 
+print(f"Processing {input_path}")
 # process sound file
 df = smile.process_file(input_path)
+print(f'Done')
 
 # save csv
 df.to_csv(os.path.join(output_dir, output_filename), index=False)
+print(f'Saved to {os.path.join(output_dir, output_filename)}')
